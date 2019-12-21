a92729e92a0c2126b61d4142fac79de4eac949ce
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2019-12-21 23:02:11 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -4,7 +4,10 @@ epic_tragedy = {
     :patriarch => {name: "Lord Montague", age: "53"},
     :matriarch => {name: "Lady Montague", age: "54"},
     :hero => {name: "Romeo", age: "15", status: "alive"},
-    :hero_friends => []
+    :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
    },
   :capulet => {
     :patriarch => {name: "Lord Capulet", age: "50"},
